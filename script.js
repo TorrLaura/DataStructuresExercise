7a030768d9932bb4d0bfdf7b96c2c23b38f1e39d
“TorrLaura” <laplebe35@gmail.com>
2021-09-30 07:42:05 -0500

    completed DataStructuresExercise

---
+++ script.js
@@ -0,0 +1,12 @@
+const jedi = [];
+console.log(jedi);
+
+jedi[0] = "Luke";
+console.log(jedi);
+
+const arrayExam = jedi.push ("Obi-Wan Kenob");
+console.log(arrayExam);
+console.log(jedi);
+
+const arrayExam = jedi.unshift("Yoda");
+console.log(arrayExam);
